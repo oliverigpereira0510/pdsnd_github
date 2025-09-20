18e6b0af7c4b373e7eb6b079626ac822184e9eb9
Oliver Pereira <oliverigpereira@gmail.com>
2025-09-20 11:22:12 +0400

    Updated the comments for user stats

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -100,7 +100,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays user statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
